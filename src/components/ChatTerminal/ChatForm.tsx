16fe6b10dc09c22c1c507a4ff612d9b4f68a86f9
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-09 21:09:30 +0800

    feat: update chatbody and chatform components

--- src/components/ChatTerminal/ChatForm.tsx
+++ src/components/ChatTerminal/ChatForm.tsx
@@ -39,15 +39,14 @@ const ChatForm: React.FC = () => {
     }
   }
 
-
   return (
-    <div className="absolute bottom-0 z-20 w-full">
+    <div className="flex w-full">
       <form onSubmit={handleSubmit(sendMessage)} className="flex flex-row items-center w-full bg-cyber-black">
         <div
           contentEditable
           id="chatbox"
-          className={`${ isSubmitting ? 'hidden' : 'block' } w-full h-full max-h-[5rem] px-5 py-5 overflow-y-auto whitespace-pre-wrap text-xs cursor-text focus:outline-none font-light`}
-          placeholder="Message here..."
+          className={`${ isSubmitting ? 'hidden' : 'block' } w-full h-full max-h-[5rem] px-5 py-5 overflow-y-auto whitespace-pre-wrap text-sm cursor-text focus:outline-none font-light`}
+          placeholder=">> Message here..."
           onInput={(e: any) => setValue('message_box', e.currentTarget.textContent, { shouldValidate: true })}
           onKeyPress={handleKeyPress}
         />
@@ -55,9 +54,9 @@ const ChatForm: React.FC = () => {
         <div className="flex flex-row items-center justify-end px-5 space-x-3">
           {isSubmitting
             ?
-            'Loading...'
+            <LoadingButton />
             :
-            <button type="submit" className="flex flex-row justify-end w-full text-modern-white opacity-30 transition ease-in-out duration-300 hover:scale-95 disabled:cursor-not-allowed disabled:opacity-50" disabled={isSubmitting}>
+            <button type="submit" className="flex flex-row justify-end w-full text-cyber-green transition ease-in-out duration-300 hover:scale-95">
               <svg className="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z"/>
               </svg>
@@ -70,3 +69,27 @@ const ChatForm: React.FC = () => {
 }
 
 export default ChatForm
+
+function LoadingButton() {
+  return (
+    <div className="flex">
+      <svg width="38px" height="38px" viewBox="0 0 45 45" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" color="#00FE00">
+        <g fill="none" fill-Rule="evenodd" transform="translate(1 1)" stroke-Width="2">
+          <circle cx="22" cy="22" r="6">
+            <animate attributeName="r" begin="1.5s" dur="3s" values="6;22" calcMode="linear" repeatCount="indefinite"></animate>
+            <animate attributeName="stroke-opacity" begin="1.5s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite"></animate>
+            <animate attributeName="stroke-width" begin="1.5s" dur="3s" values="2;0" calcMode="linear" repeatCount="indefinite"></animate>
+          </circle>
+          <circle cx="22" cy="22" r="6">
+            <animate attributeName="r" begin="3s" dur="3s" values="6;22" calcMode="linear" repeatCount="indefinite"></animate>
+            <animate attributeName="stroke-opacity" begin="3s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite"></animate>
+            <animate attributeName="stroke-width" begin="3s" dur="3s" values="2;0" calcMode="linear" repeatCount="indefinite"></animate>
+          </circle>
+          <circle cx="22" cy="22" r="8">
+            <animate attributeName="r" begin="0s" dur="1.5s" values="6;1;2;3;4;5;6" calcMode="linear" repeatCount="indefinite"></animate>
+          </circle>
+        </g>
+      </svg>
+    </div>
+  )
+}
