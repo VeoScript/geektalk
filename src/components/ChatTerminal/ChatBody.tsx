2f889ba350d741a11cc82a7ef61d5a56f0dd4c80
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-10 11:40:11 +0800

    chore: change > to &gt; (#19)

--- src/components/ChatTerminal/ChatBody.tsx
+++ src/components/ChatTerminal/ChatBody.tsx
@@ -5,12 +5,12 @@ const ChatBody: React.FC = () => {
     <div className="relative w-full h-full overflow-y-auto overflow-x-hidden bg-cyber-dim">
       <div className="absolute bottom-3 left-3 flex flex-col w-full">
         <div className="flex flex-row items-center w-full space-x-2">
-          <span className="font-normal text-cyber-white text-opacity-30">{`>`}</span>
+          <span className="font-normal text-cyber-white text-opacity-30">&gt;</span>
           <span className="font-light text-sm text-cyber-white">little_boy <span className="text-cyber-white">:</span></span>
           <span className="font-light text-sm text-cyber-green">can you teach me how to hack?</span>
         </div>
         <div className="flex flex-row items-center w-full space-x-2">
-          <span className="font-normal text-cyber-white text-opacity-30">{`>`}</span>
+          <span className="font-normal text-cyber-white text-opacity-30">&gt;</span>
           <span className="font-light text-sm text-cyber-violet">unknown_hacker <span className="text-cyber-white">:</span></span>
           <span className="font-light text-sm text-cyber-green">sure why not</span>
         </div>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,15 +5,15 @@ const SideBar: React.FC = () => {
   return (
     <div className="flex flex-col w-full h-full py-5 space-y-5">
       <div className="flex flex-col w-full max-w-xl px-3 space-y-1">
-        <h1 className="font-bold text-xl text-cyber-white">{`>`} GeekTalk💻</h1>
+        <h1 className="font-bold text-xl text-cyber-white">&gt; GeekTalk💻</h1>
       </div>
       <div className="flex flex-col w-full space-y-2">
         <div className="flex flex-row items-center justify-between w-full px-3">
           <Link href="/">
-            <a className="font-light text-xs text-cyber-white text-opacity-80 hover:underline">{`>`} Discover Servers</a>
+            <a className="font-light text-xs text-cyber-white text-opacity-80 hover:underline">&gt; Discover Servers</a>
           </Link>
           <Link href="/signin">
-            <a className="font-light text-xs text-red-500 hover:underline"><span className="text-cyber-white text-opacity-50">{`>`}</span> Signout</a>
+            <a className="font-light text-xs text-red-500 hover:underline"><span className="text-cyber-white text-opacity-50">&gt;</span> Signout</a>
           </Link>
         </div>
         <span className="border-b border-cyber-white border-opacity-10" />
@@ -21,7 +21,7 @@ const SideBar: React.FC = () => {
       <div className="flex flex-col w-full h-full px-3 space-y-2">
         <div className="flex flex-row items-center justify-between w-full">
           <h5 className="font-light text-sm"><span className="text-cyber-white">$</span> Server Name</h5>
-          <button className="font-light text-xs text-cyber-white hover:underline">{`> Chat`}</button>
+          <button className="font-light text-xs text-cyber-white hover:underline">&gt; Chat</button>
         </div>
       </div>
     </div>
