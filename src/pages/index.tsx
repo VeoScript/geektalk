1519b8119af8d13c2703cf6959f04f6601fc8522
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-21 12:09:24 +0800

    wip: add server page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,15 @@
-import type { NextPage } from 'next'
 import React from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
-import Layout from '~/layouts/default'
-import ChatHeader from '~/components/ChatTerminal/ChatHeader'
-import ChatBody from '~/components/ChatTerminal/ChatBody'
-import ChatForm from '~/components/ChatTerminal/ChatForm'
-import ChatParticipants from '~/components/ChatTerminal/ChatParticipants'
+import { useRouter } from 'next/router'
 
-const Home: NextPage = () => {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>GeekTalk</title>
-      </Head>
-      <Layout>
-        <div className="relative flex flex-row w-full max-w-full h-full overflow-hidden">
-          <div className="flex flex-col w-full">
-            <ChatHeader />
-            <ChatBody />
-            <ChatForm />
-          </div>
-          <div className="flex flex-col w-full max-w-[15rem] border-l border-cyber-white border-opacity-10 bg-cyber-black">
-            <ChatParticipants />
-          </div>
-        </div>
-      </Layout>
-    </React.Fragment>
+export default function Index() {
+  const router = useRouter()
+  React.useEffect(() => {
+    router.push('/discover-servers')
+  })
+  return(
+    <Head>
+      <title>GeekTalk</title>
+    </Head>
   )
 }
-
-export default Home
---
+++ src/pages/server.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next'
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '~/layouts/default'
+import ChatHeader from '~/components/ChatTerminal/ChatHeader'
+import ChatBody from '~/components/ChatTerminal/ChatBody'
+import ChatForm from '~/components/ChatTerminal/ChatForm'
+import ChatParticipants from '~/components/ChatTerminal/ChatParticipants'
+
+const Server: NextPage = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>GeekTalk</title>
+      </Head>
+      <Layout>
+        <div className="relative flex flex-row w-full max-w-full h-full overflow-hidden">
+          <div className="flex flex-col w-full">
+            <ChatHeader />
+            <ChatBody />
+            <ChatForm />
+          </div>
+          <div className="flex flex-col w-full max-w-[15rem] border-l border-cyber-white border-opacity-10 bg-cyber-black">
+            <ChatParticipants />
+          </div>
+        </div>
+      </Layout>
+    </React.Fragment>
+  )
+}
+
+export default Server
