a363f5d668b24967b90df08f0a57ab30f174daa4
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-21 13:22:37 +0800

    feat: add prisma and @prisma/client dependecies (#24)

--- src/components/CreateServer/ServerHeader.tsx
+++ src/components/CreateServer/ServerHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreateServerHeader: React.FC = () => {
   return (
     <div className="flex flex-row items-center justify-between w-full px-5 py-2 border-b border-cyber-dim">
-      <h5 className="font-normal text-lg"><span className="text-cyber-white text-opacity-30">&gt;</span> Create Server</h5>
+      <h5 className="font-normal text-base"><span className="text-cyber-white text-opacity-50">&gt;</span> Create Server</h5>
       <input
         className="font-light text-sm px-3 py-4 w-full max-w-xs bg-cyber-black border border-cyber-white border-opacity-20 focus:border-cyber-green focus:outline-none"
         type="text"
