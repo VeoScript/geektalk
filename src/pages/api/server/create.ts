4c86259b6793d835c44aa96e1a57b81dace17f23
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-23 14:59:23 +0800

    feat: add create server functionalities and api routes

--- src/pages/api/server/create.ts
+++ src/pages/api/server/create.ts
@@ -1,9 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '~/lib/Prisma'
 
-type Data = {
-  name: string
+interface Server {
+  name: String
+  date: Date
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'Geek Talk Developers' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Server>) {
+  const server_name = req.body.server_name
+  const date = new Date()
+  const create_server = await prisma.server.create({
+    data: {
+      name: server_name,
+      date: date
+    }
+  })
+  res.status(200).json(create_server)
 }
