259bb345d80bcd9db55bebfbcbef727126d89d8e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-25 10:44:57 +0800

    feat: add join private and public server button (#52)

--- src/components/ServersTerminal/ServerBody.tsx
+++ src/components/ServersTerminal/ServerBody.tsx
@@ -27,11 +27,15 @@ const ServerBody: React.FC<GeekProps> = ({ servers }) => {
     <div className="flex flex-col w-full h-full overflow-y-auto bg-cyber-dim">
       {server_data.map((server: any, i: any) => (
         <div className="flex flex-row justify-between w-full border-b border-cyber-white border-opacity-10" key={i}>
-          <div className="flex w-full text-cyber-white text-opacity-80 text-base px-3 py-5">
+          <div className={`${(server.status === 'public' || server.status === 'Public' || server.status === 'PUBLIC') ? `text-cyber-yellow` : `text-cyber-white`} flex w-full text-base text-opacity-80 px-3 py-5`}>
             <span className="text-cyber-white text-opacity-50">$</span>&nbsp;{ server.name }
           </div>
-          <JoinPublic server={server} />
-          {/* <JoinPrivate server={server} /> */}
+          {(server.status === 'public' || server.status === 'Public' || server.status === 'PUBLIC') && (
+            <JoinPublic server={server} />
+          )}
+          {(server.status === 'private' || server.status === 'Private' || server.status === 'PRIVATE') && (
+            <JoinPrivate server={server} />
+          )}
         </div>
       ))}
     </div>
