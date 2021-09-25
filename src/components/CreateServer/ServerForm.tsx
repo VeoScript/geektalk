777ed54573582be2cbaaffc014636229d54171da
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-25 09:41:01 +0800

    feat: create functionalities for public and private server-status

--- src/components/CreateServer/ServerForm.tsx
+++ src/components/CreateServer/ServerForm.tsx
@@ -7,6 +7,8 @@ type GeekProps = {
 }
 
 interface FormData {
+  server_status: string
+  server_passcode: string
   server_name: string
 }
 
@@ -25,17 +27,48 @@ const CreateServerForm: React.FC<GeekProps> = ({ host }) => {
     refreshInterval: 1000
   })
 
-  const { register, handleSubmit, setError, reset, formState: { errors, isSubmitting } } = useForm<FormData>()
+  const { register, handleSubmit, setError, reset, clearErrors, formState: { errors, isSubmitting } } = useForm<FormData>()
+
+  const [serverStatus, setServerStatus] = React.useState(false)
+
+  function onPublic() {
+    setServerStatus(false)
+  }
+
+  function onPrivate() {
+    setServerStatus(true)
+  }
+
+  function changeStatus(e: any) {
+    if (e.target.value === '') {
+      clearErrors('server_name')
+    }
+    if (e.target.value === 'Private' || e.target.value === 'PRIVATE' || e.target.value === 'private' || e.target.value === 'Public' || e.target.value === 'PUBLIC' || e.target.value === 'public') {
+      if (e.target.value === 'Private' || e.target.value === 'private' || e.target.value === 'PRIVATE') {
+        onPrivate()
+      } else {
+        onPublic()
+      }
+      clearErrors('server_name')
+    } else {
+      setError('server_name', {
+        type: 'manual',
+        message: 'Invalid server status, type Public or Private only.',
+      })
+    }
+  }
 
   async function handleCreate(formData: FormData) {
     const user_id = host.id
+    const server_status = formData.server_status
+    const server_passcode =  formData.server_passcode
     const server_name = formData.server_name
     const check_server = servers.find((server: { name: string }) => server.name === server_name)
 
     if (check_server) {
       setError('server_name', {
-        type: "manual",
-        message: "Server name already exist.",
+        type: 'manual',
+        message: 'Server name already exist.',
       })
       return
     }
@@ -47,16 +80,63 @@ const CreateServerForm: React.FC<GeekProps> = ({ host }) => {
       },
       body: JSON.stringify({
         user_id,
+        server_status,
+        server_passcode,
         server_name
       })
     })
     reset()
+    onPublic()
     mutate(`/api/server/get/created_servers/${host.id}`)
   }
 
   return (
     <div className="flex flex-col w-full py-5 px-3 bg-cyber-dim border-b border-cyber-white border-opacity-10">
-      <form onSubmit={handleSubmit(handleCreate)} className="flex flex-row items-center justify-between w-full">
+      <form onSubmit={handleSubmit(handleCreate)} className="flex flex-col items-center justify-between w-full">
+        {/* -- server status text-input -- */}
+        <div className="flex flex-row items-center w-full font-light text-base">
+          <span className="text-cyber-white text-opacity-50">&gt;</span>&nbsp;Server Status&nbsp;
+          <span className="text-cyber-white text-opacity-50"> : </span>&nbsp;
+          {!isSubmitting && (
+            <input
+              className="font-light text-cyber-white bg-cyber-dim focus:outline-none"
+              type="text"
+              placeholder="Type Public or Private..."
+              {...register("server_status", {
+                required: true
+              })}
+              onChange={changeStatus}
+            />
+          )}
+          {isSubmitting && (
+            <div className="font-light text-cyber-white bg-cyber-dim focus:outline-none">
+              Wait...
+            </div>
+          )}
+        </div>
+        {/* -- server passcode text-input -- */}
+        {serverStatus && (
+          <div className="flex flex-row items-center w-full font-light text-base">
+            <span className="text-cyber-white text-opacity-50">&gt;</span>&nbsp;Passcode&nbsp;
+            <span className="text-cyber-white text-opacity-50"> : </span>&nbsp;
+            {!isSubmitting && (
+              <input
+                className="font-light text-cyber-white bg-cyber-dim focus:outline-none"
+                type="password"
+                placeholder="Enter passcode"
+                {...register("server_passcode", {
+                  required: true
+                })}
+              />
+            )}
+            {isSubmitting && (
+              <div className="font-light text-cyber-white bg-cyber-dim focus:outline-none">
+                Wait...
+              </div>
+            )}
+          </div>
+        )}
+        {/* -- server name text-input -- */}
         <div className="flex flex-row items-center w-full font-light text-base">
           <span className="text-cyber-white text-opacity-50">&gt;</span>&nbsp;Enter Server Name&nbsp;
           <span className="text-cyber-white text-opacity-50"> : </span>&nbsp;
--- src/pages/api/server/post/create_server.ts
+++ src/pages/api/server/post/create_server.ts
@@ -4,17 +4,23 @@ import prisma from '~/lib/Prisma'
 interface Server {
   name: String
   date: Date
+  status: String
+  passcode?: any
   userId: String
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Server>) {
   const server_name = req.body.server_name
+  const server_status = req.body.server_status
+  const server_passcode = req.body.server_passcode
   const user_id = req.body.user_id
   const date = new Date()
   const create_server = await prisma.server.create({
     data: {
       name: server_name,
       date: date,
+      status: server_status,
+      passcode: server_passcode,
       userId: user_id
     }
   })
