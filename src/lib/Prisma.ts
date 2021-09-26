3b327ad2678551a6b4ed1969f9022dc63192579e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-26 12:55:18 +0800

    refactor: update prisma global variable

--- src/lib/Prisma.ts
+++ src/lib/Prisma.ts
@@ -12,14 +12,8 @@ interface CustomNodeJsGlobal extends NodeJS.Global {
 
 declare const global: CustomNodeJsGlobal
 
-let prisma
+const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
+
 export default prisma
